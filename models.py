d1c1f2a94396369056896053d7243e3c5c78baa6
Brian Weber <brianweber2@gmail.com>
2017-03-25 15:49:48 -0700

    Initial commit. Basic CRUD operations working on Todos.

---
+++ models.py
@@ -0,0 +1,25 @@
+import datetime
+
+from argon2 import PasswordHasher
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer,
+                          BadSignature, SignatureExpired)
+from peewee import *
+
+import config
+
+DATABASE = SqliteDatabase('todos.sqlite')
+
+
+class Todo(Model):
+    name = CharField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = DATABASE
+
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([Todo], safe=True)
+    # DATABASE.create_tables([User, Todo], safe=True)
+    DATABASE.close()
